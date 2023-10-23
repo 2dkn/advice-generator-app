c4f53331138603cd0bb12679bf564d3094b6b4d7
2dkn <2dkneely@gmail.com>
2023-10-23 07:47:52 -0400

    Finished Javascript part more css and media queries next

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 "strict";
 
-const API = "https://api.adviceslip.com";
+const api = "https://api.adviceslip.com";
 const button = document.getElementById("next-btn");
+const advice = document.getElementById("advice-text");
+const number = document.getElementById("advice-number");
+const adviceText = document.getElementById("advice-text");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 224) + 1;
+};
+
+const fetchAdvice = () => {
+  const randomAdviceNumber = getRandomNumber();
+
+  // Fetch advice text
+  fetch(`${api}/advice/${randomAdviceNumber}`)
+    .then((response) => response.json())
+    .then((data) => {
+      advice.innerText = data.slip.advice;
+    })
+    .catch((error) => {
+      console.error("Error fetching advice: " + error);
+    });
+
+  // Display the advice number
+  number.innerText = `ADVICE #${randomAdviceNumber}`;
+};
+
+// Add a click event listener to the button to fetch advice on click
+button.addEventListener("click", fetchAdvice);
+
+fetchAdvice();
